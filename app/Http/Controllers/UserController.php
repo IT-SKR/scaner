2c8fa1f28800392a0f3a465f817cf09e377896c0
liuwei-pbj@hfax.com <liuwei-pbj@hfax.com>
2019-12-09 22:39:50 +0800

    提交一个版本试试

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Itskr\SkrLaravel\Skr;
 
 class UserController extends Controller
@@ -11,8 +15,49 @@ class UserController extends Controller
     //
     public function scx_login(Request $request){
 
+        Log::info('sxc_login_code:',$request->all());
+        Skr::check($request->all(),['code'=>['required|max:30']]);
+        Log::info('sxc_login_code_checked');
 
-        return Skr::response('SUCCESS');
+        $http = new Client();
+
+        $response = $http->get('https://api.weixin.qq.com/sns/jscode2session',[
+            'query'=>[
+                'appid'=>config('weichat.appid'),
+                'secret'=>config('weichat.secret'),
+                'js_code'=>$request->code,
+                'grant_type'=>'authorization_code']
+        ]);
+
+        if ($response->getStatusCode() != 200){
+            return Skr::response('DEFAULT');
+        }
+
+        $data = json_decode((string) $response->getBody(), true);
+        Log::info('sxc_login_code_success:',$data);
+
+
+        if ($data['errcode'] !== 0){
+            return Skr::response('DEFAULT',$data);
+        }
+
+
+        //如果成功拿到用户信息，则给用户进行注册，注册的密码统一用sxc_123456
+
+        $user = User::where('openid',$data['openid'])->first();
+
+        //用户为空，进行注册
+        if (empty($user)){
+            User::create([
+                'openid' => $data['openid'],
+                'password' => Hash::make('sxc_123456'),
+            ]);
+        }
+
+        return Skr::response('SUCCESS',[
+            'access_token'=>'success',
+            'openid'=>$data['openid']
+        ]);
     }
 
 
---
+++ config/weichat.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'appid' => env('SXC_APPID'),
+    'secret' => env('SXC_SECRET'),
+];
